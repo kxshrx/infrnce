a4992a8b0f947b7f2af6078304f816f8a69c62ef
Kishore <kishore2k26@gmail.com>
2025-07-20 09:23:47 +0530

    Add module docstrings for log classification system components

---
+++ log_classification_system/__init__.py
@@ -0,0 +1,8 @@
+"""
+Log Classification System
+
+A hybrid intelligent log classification system for OpenStack infrastructure logs.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Log Classification Team"
