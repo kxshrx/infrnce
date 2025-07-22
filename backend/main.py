3e3b82004726ce01d545a77b5601dcaa3f5196c2
Kishore <kishore2k26@gmail.com>
2025-07-22 23:59:38 +0530

    feat: Update CORS settings to allow requests from the production frontend

--- backend/main.py
+++ backend/main.py
@@ -51,7 +51,7 @@ app = FastAPI(
 # Enable CORS for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://infrnce.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
